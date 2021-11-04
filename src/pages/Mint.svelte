fcc1ed96cf2cf7e78772329e1981604e58493905
robit <36677806+robit-man@users.noreply.github.com>
2021-11-04 08:33:48 -0700

    Update Mint.svelte

--- src/pages/Mint.svelte
+++ src/pages/Mint.svelte
@@ -78,4 +78,8 @@
       top:calc(4rem + 100px);
       height:calc(100vh - 150px - 4rem);
     }
+	@media screen and (max-width:900px){
+	.wrapper{position:relative;margin:2rem auto!important;top:auto!important;height:auto!important;}
+	}
+	
 </style>
