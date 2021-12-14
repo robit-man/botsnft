6731494146d8b0378905c465a42463d38144950b
robit <36677806+robit-man@users.noreply.github.com>
2021-12-14 16:38:29 +0000

    menu

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,21 @@
     import { Router, Route, Link } from "svelte-routing";
     import Home from "./pages/Home.svelte";
     import Mint from "./pages/Mint.svelte";
-    import Intel from "./pages/Intel.svelte";
-    import Four from "./pages/Four.svelte";
-    import { fly, fade, slide } from 'svelte/transition';
-    import Gallery from "./pages/Gallery.svelte"
+    import Roadmap from "./pages/Roadmap.svelte";
+    import Four from "./pages/Four.svelte";    
+    import Gallery from "./pages/Gallery.svelte";
     export let url = '';
+
     import { address, contract, provider, nfts, balances } from './store';
     import { setContext } from 'svelte';
     import { writable } from 'svelte/store';
-    import { Audio, Video } from 'svelte-audio-video'
+    import { Audio, Video } from 'svelte-audio-video';
     import { 
         initProvider,
         mintPepe,
-    } from './utils.js';
+    } from './utils.js';  
+    
+
   const track = {
     attach: (element, _track) => {
       // when fired, do whatever is needed to attach the track
@@ -35,6 +37,13 @@
         }
     }
     
+    async function toggle(event){
+      document.getElementById("sidebar").classList.toggle('active');
+      document.getElementById("window").classList.toggle('active2');
+      document.getElementById("menu1").classList.toggle('active3');
+      document.getElementById("menu2").classList.toggle('active4');
+    }
+    
     function connect(event) {
         connectEthProvider(false);
     }    
@@ -47,13 +56,6 @@
     //export const innerWidth = writable(1000) Any use for these
     setContext('app', app);
     console.log(window.location.href);
-
-    function toggle(event){
-      document.getElementById("sidebar").classList.toggle('active');
-      document.getElementById("window").classList.toggle('active2');
-      document.getElementById("menu1").classList.toggle('active3');
-      document.getElementById("menu2").classList.toggle('active4');
-    }
 </script>
 
 <div class="main">
@@ -62,34 +64,41 @@
           src="/"
           type="audio/mp3" />
       </Audio>
-      <div class="menu" on:click={toggle} style="position: fixed;z-index: 10;transition: all 0.34s;position: relative;width: 45px;height: 45px;border-radius: 100%;cursor: pointer;"><div class="menu_part" id="menu1" style=""></div><div class="menu_part" id="menu2" style=""></div></div>
-    <Router url="{url}">
-      <header id="sidebar">
-        <div class="left-box">
-                  <Link to="/"><img src="/imgs/impossible-triangle.svg" width="100px" height="100px" alt=""></Link>
-        </div>
-        <nav>
-          <Link to="mint"><p transition:fade="{{ y: 200, duration: 2000 }}" style="font-size:1.8rem;margin:0rem 1rem;">Mint</p></Link>
-          <Link to="gallery"><p transition:fade="{{ y: 300, duration: 2000 }}" style="font-size:1.8rem;margin:0rem 1rem;">Gallery</p></Link>
-          <Link to="intel"><p transition:fade="{{ y: 400, duration: 2000 }}" style="font-size:1.8rem;margin:0rem 1rem;">Intel</p></Link>
-        </nav>
-        <div class="right-box">
-          {#if !$address}
-          <button on:click={connect} class="mint-button"></button>
-        {:else}
-        <a href="/mint"><button class="mint-button"><p>Go To Mint</p></button></a>
-        {/if} 
-        </div>
-      </header>
-	    <div class="router-window" id="window">
-        <Route path="/"><Home /></Route>
-        <Route path="/mint" component={Mint}><Mint /></Route>
-        <Route path="/mint"><Mint /></Route>
-        <Route path="/intel" component={Intel}><Intel /></Route>
-        <Route path="/gallery" component={Gallery}></Route>
-        <Route path="/*"><Four /></Route>
-      </div>
-    </Router>
+      <div class="menu" on:click={toggle} style="position: fixed;z-index: 10;transition: all 0.34s;position: relative;width: 2rem;height: 2rem;border-radius: 100%;cursor: pointer;"><div class="menu_part" id="menu1" style=""></div><div class="menu_part" id="menu2" style=""></div></div>
+
+      <Router url="{url}">
+        
+        <header id="sidebar">
+          <div class="left-box">
+                    <a href="#hero"><img src="/imgs/impossible-triangle.svg" width="50px" height="50px" alt=""></a>
+          </div>
+
+          <nav>
+             <!--<Link to="mint"><p on:click={toggle} style="font-size:1.8rem;margin:0rem 1rem;">Mint</p></Link>
+            <Link to="gallery"><p on:click={toggle} style="font-size:1.8rem;margin:0rem 1rem;">Gallery</p></Link>
+            <Link to="roadmap"><p on:click={toggle} style="font-size:1.8rem;margin:0rem 1rem;">Roadmap</p></Link>
+           -->
+            <a href="#about"><p on:click={toggle} style="font-size:1.8rem;margin:0rem 1rem;">About</p></a>
+            <a href="#mint"><p on:click={toggle} style="font-size:1.8rem;margin:0rem 1rem;">Mint</p></a>
+            <a href="#roadmap"><p on:click={toggle} style="font-size:1.8rem;margin:0rem 1rem;">Roadmap</p></a>
+          </nav>
+
+          <div class="right-box">
+            {#if !$address}
+            <button on:click={connect} class="mint-button"></button>
+          {:else}
+          <a href="/mint"><button class="mint-button"><p>Go To Mint</p></button></a>
+          {/if} 
+          </div>
+        </header>
+
+        <div class="router-window" id="window">
+          <Route path="/"><Home /></Route>
+          <Route path="/mint" component={Mint}><Mint /></Route>
+          <Route path="/gallery" component={Gallery}><Gallery /></Route>
+          <Route path="/*" component={Four}><Four /></Route>
+        </div> 
+      </Router>
 
     <script type="text/javascript" src="/script.js"></script>
     <footer>
@@ -108,12 +117,20 @@
     left:-164px;transition:all 0.2s ease;
     top:0px;position:fixed;
   }
+  
+
+.color1 { fill: #ccc; }
+
+.color2 { fill: #ddd; }
+
+.color3 { fill: #eee; }
+
 .mint-button{
 	margin:auto;
-  width:100px;
+  width:50px;
 	min-width:auto;
 	cursor:pointer;
-  height:100px;
+  height:50px;
   border-radius:4px;
   border:0px solid black;
   transition:all 0.2s ease;
